df51064a87037f8c1705c21d4a2c260329faacb7
Eric Millen <ericdelmermillen@gmail.com>
2026-09-17 11:22:18 +0100

    refactored _mixin.scss and fixed LoginPage and NotFoundPage iiners

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,12 +4,14 @@ const NotFoundPage = () => {
   
   return (
     <div className="notFoundPage">
-      <h1 className="notFoundPage__heading">
-        Error 404
-      </h1>
-      <h2 className="notFoundPage__sub-heading">
-        Nothing beautiful to see here...
-      </h2>
+      <div className="notFoundPage__inner">
+        <h1 className="notFoundPage__heading">
+          Error 404
+        </h1>
+        <h2 className="notFoundPage__sub-heading">
+          Nothing beautiful to see here...
+        </h2>
+      </div>
     </div>
   )};
 
